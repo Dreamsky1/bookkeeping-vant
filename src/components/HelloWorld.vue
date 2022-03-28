12c297e0ab4fd8d7cace8cc87fc127febb403144
gxb58 <742081415@qq.com>
2022-03-28 16:10:43 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,21 +12,12 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="xui-test">
+      <div class="xui-center">测试之前的业务</div>
+    </div>
+    <div class="xui-name">
+      <div class="xui-center">nice</div>
+    </div>
   </div>
 </template>
 
@@ -35,12 +26,26 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+@import 'src/scss/mixins';
+body{
+  margin: 0;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +59,19 @@ li {
 }
 a {
   color: #42b983;
+}
+.xui-test{
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  @include text-nowrap(100px);
+  .xui-center{
+    font-size: 20px;
+    color: red;
+  }
+}
+.xui-name{
+  margin-left: auto;
+  @extend .xui-test;
 }
 </style>

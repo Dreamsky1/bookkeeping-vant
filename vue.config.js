74395fec6cc8f9501e7b80d2f249a8809aad36d5
gxb58 <742081415@qq.com>
2022-04-01 20:00:43 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       '/api2': { // 匹配所有以'/api'开头的请求路径
         target: 'http://127.0.0.1:80', // 代理目标的基础路径
         changeOrigin: true,
-        pathRewrite: { '^/api2': '' }
+        pathRewrite: { '^/api2': 'api' }
       },
     }
   }

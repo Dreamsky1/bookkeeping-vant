32d3bb1a697bf24a8d95e1ad351ac79cfeb6a98d
gxb58 <742081415@qq.com>
2022-04-02 18:23:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
+    <transition name="page-go">
+      <router-view class="page"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -15,11 +17,59 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
+  },
+
+  computed: {
+    direction () {
+      const currentPath = this.$route.path;
+      const localRoute = (window.vue && window.vue.localRoute) || [];
+      const index = localRoute.indexOf(currentPath);
+      let tranName = '';
+
+      if (localRoute.length === 0) {
+        tranName = 'fade'; // 首页，渐显
+      } else if (index >= 0) {
+        tranName = 'page-back'; // 回退，右划
+      } else {
+        tranName = 'page-go'; // 进入新页面，左划
+      }
+      return tranName;
+    }
   }
 }
 </script>
 
 <style>
+.page{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition: all 0.2s ease-in-out;
+}
+.page-go-enter-active {
+  transform: translate3d(100%, 0, 0);
+}
+.page-go-enter-to {
+  transform: translate3d(0, 0, 0);
+}
+.page-go-leave-active {
+  transform: translate3d(0, 0, 0);
+}
+.page-go-leave-to {
+  transform: translate3d(-100%, 0, 0);
+}
+.page-back-enter-active {
+  transform: translate3d(-100%, 0, 0);
+}
+.page-back-enter-to {
+  transform: translate3d(0, 0, 0);
+}
+.page-back-leave-active {
+  transform: translate3d(0, 0, 0);
+}
+.page-back-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/router.js
+++ src/router/router.js
@@ -8,5 +8,27 @@ const router = new VueRouter({
   routes: routers
 })
 
+// 路由拦截 router.js
+router.afterEach((to, from) => {
+  // console.log(to, from);
+  if (!(from.path === '/' && from.name === null)) {
+    setLocalRoute(to, from)
+  }
+});
+
+function setLocalRoute(to, from) {
+  // 本地已访问页面路由,存5条
+  const localRoute = window.vue.localRoute = window.vue.localRoute || [];
+  console.log(localRoute);
+  const from_index = localRoute.indexOf(from.path);
+  const to_index = localRoute.indexOf(to.path);
+  if (from_index < 0) {
+    localRoute.unshift(from.path);
+    to_index >= 0 && localRoute.splice(to_index, 1)
+  }
+  if (localRoute.length > 5) {
+    localRoute.splice(0, 1)
+  }
+}
 
 export default router
